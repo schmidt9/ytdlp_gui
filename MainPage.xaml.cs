6d1af820f7791cc6737972f9ff3b0e4f36e45a8a
Your Name <you@example.com>
2026-09-20 22:27:06 +0300

    add controls;

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -2,7 +2,6 @@
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
 
 	public MainPage()
 	{
@@ -11,7 +10,58 @@ public partial class MainPage : ContentPage
 
 	private void OnStartDownloadClicked(object sender, EventArgs e)
 	{
-		// Start the download process here
+		if (!ValidateUrlEntry() || !ValidateSavePathEntry())
+		{
+			return;
+		}
+
+
+	}
+
+	private void OnSelectSavePathClicked(object sender, EventArgs e)
+	{
+		SelectSavePath();
+	}
+
+	private bool ValidateUrlEntry()
+	{
+		if (string.IsNullOrWhiteSpace(UrlEntry.Text))
+		{
+			DisplayAlert("Error", "Please enter a valid YouTube URL.", "OK");
+			return false;
+		}
+
+		return true;
+	}
+
+	private bool ValidateSavePathEntry()
+	{
+		if (string.IsNullOrWhiteSpace(SavePathEntry.Text))
+		{
+			DisplayAlert("Error", "Please enter a valid save path.", "OK");
+			return false;
+		}
+
+		return true;
+	}
+
+	async void SelectSavePath()
+	{
+		try
+		{
+			var result = await FilePicker.Default.PickAsync();
+			if (result != null)
+			{
+				string fileName = result.FileName;
+				string fullPath = result.FullPath; // Путь к файлу (на некоторых ОС может быть виртуальным URI)
+
+				SavePathEntry.Text = fullPath; // Устанавливаем путь в Entry
+			}
+		}
+		catch (Exception ex)
+		{
+			await DisplayAlert("Error", $"An error occurred while picking a file: {ex.Message}", "OK");
+		}
 	}
 }
 
